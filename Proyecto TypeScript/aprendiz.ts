894b6cdf2ca53a37704cd1380934e66890c88258
SergioPardo55 <s.pardog@uniandes.edu.co>
2022-04-13 23:36:51 +0000

    Tarea 5

--- Proyecto TypeScript/aprendiz.ts
+++ Proyecto TypeScript/aprendiz.ts
@@ -1,3 +1,5 @@
+import {Curso} from './curso.js';
+
 export enum NivelEducativo{
     BACHILLERATO="Bachillerato",
     UNIVERSIDAD="Universidad",
@@ -5,7 +7,22 @@ export enum NivelEducativo{
 }
 
 export class Aprendiz{
-    constructor(public nombres:string,public apellidos:string,public avatar:string,public edad:number,public NivelEducativo:NivelEducativo){
+    constructor(public nombres:string,public apellidos:string,public avatar:string,public edad:number,public NivelEducativo:NivelEducativo,public cursos:Curso[]){
         
     }
+
+    public darCrusosCertificados():number{
+        let totalCursosC:number=0;
+        for(let index = 0; index <this.cursos.length;index++)
+        {
+            let curso: Curso = this.cursos[index];
+            if(curso.certificado)
+            {
+                totalCursosC+=1;
+            }
+        }
+        return totalCursosC;
+    }
+    
 }
+
--- Proyecto TypeScript/scripts/aprendiz.js
+++ Proyecto TypeScript/scripts/aprendiz.js
@@ -5,13 +5,24 @@ export var NivelEducativo;
     NivelEducativo["POSGRADO"] = "Posgrado";
 })(NivelEducativo || (NivelEducativo = {}));
 var Aprendiz = /** @class */ (function () {
-    function Aprendiz(nombres, apellidos, avatar, edad, NivelEducativo) {
+    function Aprendiz(nombres, apellidos, avatar, edad, NivelEducativo, cursos) {
         this.nombres = nombres;
         this.apellidos = apellidos;
         this.avatar = avatar;
         this.edad = edad;
         this.NivelEducativo = NivelEducativo;
+        this.cursos = cursos;
     }
+    Aprendiz.prototype.darCrusosCertificados = function () {
+        var totalCursosC = 0;
+        for (var index = 0; index < this.cursos.length; index++) {
+            var curso = this.cursos[index];
+            if (curso.certificado) {
+                totalCursosC += 1;
+            }
+        }
+        return totalCursosC;
+    };
     return Aprendiz;
 }());
 export { Aprendiz };
